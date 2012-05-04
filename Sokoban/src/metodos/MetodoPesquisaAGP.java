8cdaea4734b4de2652d4a3f649676b6e4a4b25fb
llino.ipl <llino.ipl@gmail.com>
2012-05-04 17:01:01 +0000

    lino: codigo que fizemos em pair-programing na sexta

--- Sokoban/src/metodos/MetodoPesquisaAGP.java
+++ Sokoban/src/metodos/MetodoPesquisaAGP.java
@@ -7,34 +7,52 @@ import agente.Solucao;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.HashMap;
+import java.util.HashSet;
 
-
-public abstract class MetodoPesquisaAGP implements MetodoPesquisa{
+public abstract class MetodoPesquisaAGP implements MetodoPesquisa {
 
     public Solucao pesquisar(Problema problema) {
         return agp(problema);
     }
 
-    public abstract void inserirSucessores(No noAExpandir, List<Estado> listaSucessores,  LinkedList<No> listaPorExandir,  Problema problema);
+    public abstract void inserirSucessores(No noAExpandir, List<Estado> listaSucessores, LinkedList<No> listaPorExandir, Problema problema);
 
     /*
-      agp(problema, metodo de pesquisa) devolve uma solucao ou falha
-      1. Criar e iniciar a lista NosPorExpandir com o estado inicial do problema.
-      2. Criar a lista vazia NosExpandidos.
-      3. Enquanto a lista NosPorExpandir não estiver vazia faz:
-         4. Remover o primeiro no, n, da lista NosPorExpandir.
-         5. Se n pertence a lista NosExpandidos, voltar ao passo 3.
-         6. Introduzir n na lista NosExpandidos.
-         7. Se n e um no objetivo, sai com sucesso, sendo a solucao obtida
-            percorrendo o caminho desde n a n0 (os arcos sao criados no passo 9).
-         8. Expandir n, gerando um conjunto S de sucessores;
-         9. Introduzir cada um dos elementos de S na lista NosPorExpandir de acordo
-            com o metodo de pesquisa. Criar arcos entre n e cada uma dos elementos de S.
-        10. Voltar ao passo 3.
-      */
-
+    agp(problema, metodo de pesquisa) devolve uma solucao ou falha
+    1. Criar e iniciar a lista NosPorExpandir com o estado inicial do problema.
+    2. Criar a lista vazia NosExpandidos.
+    3. Enquanto a lista NosPorExpandir não estiver vazia faz:
+    4. Remover o primeiro no, n, da lista NosPorExpandir.
+    5. Se n pertence a lista NosExpandidos, voltar ao passo 3.
+    6. Introduzir n na lista NosExpandidos.
+    7. Se n e um no objetivo, sai com sucesso, sendo a solucao obtida
+    percorrendo o caminho desde n a n0 (os arcos sao criados no passo 9).
+    8. Expandir n, gerando um conjunto S de sucessores;
+    9. Introduzir cada um dos elementos de S na lista NosPorExpandir de acordo
+    com o metodo de pesquisa. Criar arcos entre n e cada uma dos elementos de S.
+    10. Voltar ao passo 3.
+     */
     private Solucao agp(Problema problema) {
-        //TODO
+        LinkedList<No> nosPorExpandir = new LinkedList<No>();
+        HashSet<Estado> nosExpandidos = new HashSet<Estado>();
+
+        No no;
+        nosPorExpandir.add(new No(problema.getEstadoInicial()));
+        while (!nosPorExpandir.isEmpty()) {
+            no = nosPorExpandir.removeFirst();
+            if (nosExpandidos.contains(no.getEstado())) {
+                continue;
+            }
+
+            nosExpandidos.add(no.getEstado());
+
+            if (problema.isObjetivoAtingido(no.getEstado())) {
+                return new Solucao(problema, no);
+            }
+
+            inserirSucessores(no, problema.aplicarOperadores(no.getEstado()), nosPorExpandir, problema);
+
+        }
         return null;
     }
 }
